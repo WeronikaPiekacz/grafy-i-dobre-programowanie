9c4c685f95fdf8c2fdcb70b64942aea558680cfa
adamgwozdz00 <adamgwozdz00@gmail.com>
2025-01-15 16:04:35 +0100

    feat: Implement core logic of finding euler circuit

--- bl/graph.go
+++ bl/graph.go
@@ -2,35 +2,130 @@ package bl
 
 import (
 	"errors"
+	"github.com/google/uuid"
 )
 
-var ErrOddNumberOfEdges = errors.New("number of edges are not even")
+var ErrOddNumberOfEdges = errors.New("Graph is not eulerian circle - Number of edges are not even.")
 
 // graph.go
 type Graph struct {
 	nodes []string
 	edges []Edge
+	id    string
 }
 
-type Result struct {
-	isEulerCycle bool
-	cycle        []string
+func NewGraph(nodes []string, edges []Edge, graphId ...string) *Graph {
+	uniqueEdges := unique(edges)
+
+	if len(graphId) == 1 {
+		return &Graph{id: graphId[0], nodes: nodes, edges: uniqueEdges}
+	}
+	return &Graph{id: uuid.New().String(), nodes: nodes, edges: uniqueEdges}
 }
 
-func NewGraph(nodes []string, edges []Edge) *Graph {
-	return &Graph{nodes: nodes, edges: edges}
+func unique(edges []Edge) []Edge {
+	var uniqueEdges []Edge
+	for _, edge := range edges {
+		if !isInArray(edge, uniqueEdges) {
+			uniqueEdges = append(uniqueEdges, edge)
+		}
+	}
+	return uniqueEdges
+}
+
+func isInArray(element Edge, array []Edge) bool {
+	for _, edge := range array {
+		if edge.equals(element) {
+			return true
+		}
+	}
+	return false
 }
 
 func (graph *Graph) removeEdge(edgeToRemove Edge) {
-	// TODO: Implement
+	var newEdges []Edge
+	for _, value := range graph.edges {
+		if !value.equals(edgeToRemove) {
+			newEdges = append(newEdges, value)
+		}
+	}
+
+	graph.edges = newEdges
 }
 
 func (graph *Graph) contains(edge Edge) bool {
-	// TODO: Implement
+
+	for _, value := range graph.edges {
+		if value.equals(edge) {
+			return true
+		}
+	}
 	return false
 }
 
 func (graph *Graph) FindCircuit() ([]string, error) {
-	// TODO: Implement
-	return []string{}, nil
+	var path []string
+	if graph.hasEachNodeOddDegree() {
+		return path, ErrOddNumberOfEdges
+	}
+
+	startNode := graph.findStartVert()
+	circuitFound := false
+	return graph.solve(startNode, &path, &circuitFound), nil
+}
+
+func (graph *Graph) hasEachNodeOddDegree() bool {
+	for _, node := range graph.nodes {
+		if len(graph.getNeighboursOf(node))%2 != 0 {
+			return true
+		}
+	}
+	return false
+}
+
+func (graph *Graph) findStartVert() string {
+	for _, node := range graph.nodes {
+		numberOfEdges := len(graph.getNeighboursOf(node))
+		if numberOfEdges%2 != 0 {
+			return node
+		}
+	}
+	return graph.nodes[0]
+}
+
+func (graph *Graph) solve(startNode string, path *[]string, circuitFound *bool) []string {
+	numberOfEdges := len(graph.edges)
+	for _, node := range graph.nodes {
+		edge := *NewEdge(startNode, node)
+		if graph.contains(edge) {
+			if numberOfEdges <= 1 || !graph.isBridge(node) {
+				*path = append(*path, startNode)
+				graph.removeEdge(edge)
+				graph.solve(node, path, circuitFound)
+			}
+		}
+	}
+	if numberOfEdges == 0 && !*circuitFound {
+		*circuitFound = true
+		*path = append(*path, startNode)
+	}
+
+	return *path
+}
+
+func (graph *Graph) getNeighboursOf(node string) []string {
+	neighbours := []string{}
+	for _, value := range graph.edges {
+		if value.RightPoint == node {
+			neighbours = append(neighbours, value.LeftPoint)
+		}
+		if value.LeftPoint == node {
+			neighbours = append(neighbours, value.RightPoint)
+		}
+	}
+	return neighbours
+}
+
+func (graph *Graph) isBridge(node string) bool {
+	return len(graph.getNeighboursOf(node)) <= 1
 }
